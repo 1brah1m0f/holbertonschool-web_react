7e9a881b2c962d4308ed9f91c64eb747f86c1c47
jrosmanov <cr.osmanov@gmail.com>
2026-07-18 13:36:44 +0400

    task

--- react_intro/task_0/dashboard/src/App.jsx
+++ react_intro/task_0/dashboard/src/App.jsx
@@ -1,23 +1,22 @@
-import HbSLogo from './assets/holberton-logo.jpg'
-import './App.css'
+import React from "react";
+import holbertonLogo from "./assets/holberton-logo.jpg"; // Ensure the path is correct
+import "./App.css";
 
 function App() {
   return (
-    <>
-      <div className='App-header'>
-        <img src={HbSLogo} alt="holberton logo" />
+    <div className="App">
+      <header className="App-header">
+        <img src={holbertonLogo} alt="Holberton logo" />
         <h1>School dashboard</h1>
-      </div>
-
-      <div className='App-body'>
+      </header>
+      <div className="App-body">
         <p>Login to access the full dashboard</p>
       </div>
-
-      <div className='App-footer'>
-        <p>Copyright {new Date().getFullYear()} - holberton School</p>
-      </div>
-    </>
-  )
+      <footer className="App-footer">
+        <p>Copyright 2026 - Holberton School</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
